1ded27c898b25acde99bce8bac18bf9f172e876d
suvidha <softwaredevloper92@gmail.com>
2020-09-17 00:29:01 +0530

    new verion3 change in urls.py

--- store/admin.py
+++ store/admin.py
@@ -4,8 +4,7 @@ from .models.category import Category
 from .models.customer import Customer
 
 print('this is my new version1 change')
-print('this is my new change')
-print('this is my new change')
+
 print('this is my new change')
 
 class AdminProduct(admin.ModelAdmin):
@@ -20,4 +19,3 @@ admin.site.register(Customer)
 
 print('this is my version1 by git change')
 
-print('this is my version1 by git change')
