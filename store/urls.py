72a36e116288723156843b5fe3bf3e1ca54b55be
suvitandale <62100852+suvitandale@users.noreply.github.com>
2020-09-16 23:08:23 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import login,signup,home
 
+print('This is my 1st change')
+
 urlpatterns = [
     path('',home.index,name='homepage'),
     path('signup',signup.SignupView.as_view(),name='signup'),
